f1a0e7079c3b215c8fbd6cc1219b34a79c417347
sachin <sachinkumars1998@gmail.com>
2021-04-05 12:48:19 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product.model.ts
@@ -0,0 +1,7 @@
+export class Product {
+    ProductId:number=0;
+    ProductName:string='';
+    Price:number=0;
+    ProductPic:string='';
+    
+}
